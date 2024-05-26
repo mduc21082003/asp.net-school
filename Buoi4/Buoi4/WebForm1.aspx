d0fb4d3aa252a94239fda56afa74e64bf0db46c3
Duc Nguyen <mduc21082003@gmail.com>
2024-05-27 01:24:32 +0700

    push exercise

---
+++ Buoi4/Buoi4/WebForm1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Buoi4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            color: #00FFFF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 40px">
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 10px; top: 59px; position: absolute" Text="Mã môn"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" style="z-index: 1; left: 221px; top: 55px; position: absolute" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <asp:TextBox ID="TextBox4" runat="server" style="z-index: 1; left: 137px; top: 55px; position: absolute; width: 57px; right: 838px;"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" style="z-index: 1; left: 10px; top: 103px; position: absolute" Text="Tên môn"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server" style="z-index: 1; left: 135px; top: 103px; position: absolute"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" style="z-index: 1; left: 10px; top: 147px; position: absolute" Text="Số ĐVHT"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server" style="z-index: 1; left: 135px; top: 147px; position: absolute"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" style="z-index: 1; left: 10px; top: 190px; position: absolute; width: 130px" Text="Thêm môn học" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" style="z-index: 1; left: 152px; top: 190px; position: absolute; width: 130px" Text="Sửa môn học" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" style="z-index: 1; left: 295px; top: 189px; position: absolute; width: 130px" Text="Xóa môn học" OnClick="Button3_Click" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" style="z-index: 1; left: 10px; top: 257px; position: absolute; height: 152px; width: 232px" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Buoi4/Buoi4/WebForm1.aspx.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Buoi4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTable();
+            }
+        }
+
+        private OleDbConnection GetDatabaseConnection()
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kecprovip\Downloads\DataKTHT (2).mdb";
+            return new OleDbConnection(connectionString);
+        }
+
+
+        private void LoadTable()
+        {
+            using (OleDbConnection conn = GetDatabaseConnection())
+            {
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM MON_HOC", conn);
+                conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                DropDownList1.DataSource = reader;
+                DropDownList1.DataTextField = "MaMon";
+                DropDownList1.DataValueField = "MaMon";
+                DropDownList1.DataBind();
+            }
+            DropDownList1.Items.Insert(0, "Chọn mã môn");
+        }
+
+        private void LoadClass(string MaMon)
+        {
+            using (OleDbConnection conn = GetDatabaseConnection())
+            {
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM MON_HOC WHERE MaMon = @MaMon", conn);
+                cmd.Parameters.AddWithValue("@MaMon", MaMon);
+                conn.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                if (dt.Rows.Count > 0)
+                {
+                    TextBox2.Text = dt.Rows[0]["TenMon"].ToString();
+                    TextBox3.Text = dt.Rows[0]["SoDVHT"].ToString();
+                    conn.Close();
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chonMaMon = DropDownList1.SelectedValue;
+            if (chonMaMon != "0")
+            {
+                LoadClass(chonMaMon);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string maMon = TextBox4.Text;
+            string tenMon = TextBox2.Text;
+            string soDVHT = TextBox3.Text;
+
+            using (OleDbConnection conn = GetDatabaseConnection())
+            {
+                OleDbCommand cmd = new OleDbCommand("INSERT INTO MON_HOC (MaMon, TenMon, SoDVHT) VALUES (@MaMon, @TenMon, @SoDVHT)", conn);
+                cmd.Parameters.AddWithValue("@MaMon", maMon);
+                cmd.Parameters.AddWithValue("@TenMon", tenMon);
+                cmd.Parameters.AddWithValue("@SoDVHT", soDVHT);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            TextBox4.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+
+            LoadTable();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string maMon = DropDownList1.SelectedValue;
+            string tenMon = TextBox2.Text;
+            string soDVHT = TextBox3.Text;
+
+            using (OleDbConnection conn = GetDatabaseConnection())
+            {
+                OleDbCommand cmd = new OleDbCommand("UPDATE MON_HOC SET TenMon = @TenMon, SoDVHT = @SoDVHT WHERE MaMon = @MaMon", conn);
+                cmd.Parameters.AddWithValue("@TenMon", tenMon);
+                cmd.Parameters.AddWithValue("@SoDVHT", soDVHT);
+                cmd.Parameters.AddWithValue("@MaMon", maMon);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            LoadTable();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string maMon = DropDownList1.SelectedValue;
+            string tenMon = TextBox2.Text;
+            string soDVHT = TextBox3.Text;
+
+            using (OleDbConnection conn = GetDatabaseConnection())
+            {
+                OleDbCommand cmd = new OleDbCommand("DELETE FROM MON_HOC WHERE MaMon = @MaMon", conn);
+                cmd.Parameters.AddWithValue("@MaMon", maMon);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+
+            LoadTable();
+        }
+    }
+}
