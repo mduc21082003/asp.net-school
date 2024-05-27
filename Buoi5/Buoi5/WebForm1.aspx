6fd5d8c9bcca4f3e122f6a8d53d9460a4e655245
Duc Nguyen <mduc21082003@gmail.com>
2024-05-27 21:24:23 +0700

    push exercise

---
+++ Buoi5/Buoi5/WebForm1.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Buoi5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 10px; top: 14px; position: absolute" Text="Mã hàng"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlMaHang" runat="server" style="z-index: 1; left: 120px; top: 14px; position: absolute" OnSelectedIndexChanged="ddlMaHang_SelectedIndexChanged" AutoPostBack="True">
+            </asp:DropDownList>
+            <asp:TextBox ID="txtMaHang" runat="server" style="z-index: 1; left: 250px; top: 14px; position: absolute; width: 100px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 10px; top: 59px; position: absolute" Text="Tên hàng"></asp:Label>
+            <asp:TextBox ID="txtTenHang" runat="server" style="z-index: 1; left: 120px; top: 57px; position: absolute; right: 607px; width: 160px;"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" style="z-index: 1; left: 10px; top: 103px; position: absolute; right: 781px" Text="Đơn giá"></asp:Label>
+            <asp:TextBox ID="txtDonGia" runat="server" style="z-index: 1; left: 120px; top: 102px; position: absolute; width: 160px;"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtTimKiem" runat="server" style="z-index: 1; left: 662px; top: 191px; position: absolute"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" style="z-index: 1; left: 10px; top: 147px; position: absolute" Text="Đơn vị tính"></asp:Label>
+            <asp:TextBox ID="txtDonViTinh" runat="server" style="z-index: 1; left: 120px; top: 145px; position: absolute"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnThem" runat="server" style="z-index: 1; left: 10px; top: 191px; position: absolute; width: 75px" Text="Thêm" OnClick="btnThem_Click" />
+            <asp:Button ID="btnSua" runat="server" style="z-index: 1; left: 150px; top: 191px; position: absolute; right: 706px; width: 75px" Text="Sửa" OnClick="btnSua_Click" />
+            <asp:Button ID="btnXoa" runat="server" style="z-index: 1; left: 290px; top: 191px; position: absolute; width: 75px" Text="Xóa" OnClick="btnXoa_Click" />
+            <asp:Button ID="btnHienThi" runat="server" style="z-index: 1; left: 430px; top: 191px; position: absolute; width: 75px" Text="Hiển thị" OnClick="btnHienThi_Click" />
+            <asp:Button ID="btnTimKiem" runat="server" style="z-index: 1; left: 570px; top: 191px; position: absolute; right: 254px; width: 75px" Text="Tìm kiếm" OnClick="btnTimKiem_Click" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="gvTatCa" runat="server" style="z-index: 1; left: 10px; top: 313px; position: absolute; height: 152px; width: 817px" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical">
+                <AlternatingRowStyle BackColor="#DCDCDC" />
+                <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+                <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+                <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#0000A9" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#000065" />
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Buoi5/Buoi5/WebForm1.aspx.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Buoi5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTable();
+            }
+        }
+
+        private OleDbConnection connectDataBase()
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kecprovip\OneDrive\Documents\DataOnTap.accdb";
+            return new OleDbConnection(connectionString);
+        }
+
+        private void LoadTable()
+        {
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "SELECT * FROM Hang";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                ddlMaHang.DataSource = reader;
+                ddlMaHang.DataTextField = "MaHang";
+                ddlMaHang.DataValueField = "MaHang";
+                ddlMaHang.DataBind();
+            }
+            ddlMaHang.Items.Insert(0, "Chọn mã hàng");
+
+        }
+
+        //Load data từ dropdownlist
+        private void LoadData(string MaHang)
+        {
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "SELECT * FROM Hang WHERE MaHang = @MaHang";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaHang", MaHang);
+                conn.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                gvTatCa.DataSource = dt;
+                gvTatCa.DataBind();
+                if (dt.Rows.Count > 0)
+                {
+                    txtTenHang.Text = dt.Rows[0]["TenHang"].ToString();
+                    txtDonGia.Text = dt.Rows[0]["DonGia"].ToString();
+                    txtDonViTinh.Text = dt.Rows[0]["DonViTinh"].ToString();
+                    conn.Close();
+                }
+            }
+        }
+
+        protected void ddlMaHang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chonMaHang = ddlMaHang.SelectedValue;
+            if (chonMaHang != "0")
+            {
+                LoadData(chonMaHang);
+            }
+        }
+
+        protected void btnHienThi_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query1 = "SELECT * FROM Hang INNER JOIN PhieuXuat ON Hang.MaHang = PhieuXuat.MaHang";
+                OleDbCommand cmd1 = new OleDbCommand(query1, conn);
+
+                conn.Open();
+
+                OleDbDataAdapter adapter1 = new OleDbDataAdapter(cmd1);
+                DataTable dt1 = new DataTable();
+                adapter1.Fill(dt1);
+                gvTatCa.DataSource = dt1;
+                gvTatCa.DataBind();
+            }
+        }
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            string maHang = txtMaHang.Text;
+            string tenHang = txtTenHang.Text;
+            string donGia = txtDonGia.Text;
+            string donViTinh = txtDonViTinh.Text;
+
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "INSERT INTO Hang (maHang, tenHang, donGia, donViTinh) VALUES (@MaHang, @TenHang, @DonGia, @DonViTinh)";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaHang", maHang);
+                cmd.Parameters.AddWithValue("@TenHang", tenHang);
+                cmd.Parameters.AddWithValue("@DonGia", donGia);
+                cmd.Parameters.AddWithValue("@DonViTinh", donViTinh);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            txtMaHang.Text = String.Empty;
+            txtTenHang.Text = String.Empty;
+            txtDonGia.Text = String.Empty;
+            txtDonViTinh.Text = String.Empty;
+
+            LoadTable();
+        }
+
+        protected void btnSua_Click(object sender, EventArgs e)
+        {
+            string maHang = ddlMaHang.SelectedValue;
+            string tenHang = txtTenHang.Text;
+            string donGia = txtDonGia.Text;
+            string donViTinh = txtDonViTinh.Text;
+
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "UPDATE Hang SET TenHang = @TenHang, DonGia = @DonGia, DonViTinh = @DonViTinh WHERE MaHang = @MaHang";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@TenHang", tenHang);
+                cmd.Parameters.AddWithValue("@DonGia", donGia);
+                cmd.Parameters.AddWithValue("@DonViTinh", donViTinh);
+                cmd.Parameters.AddWithValue("@MaHang", maHang);
+
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            LoadTable();
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            string maHang = ddlMaHang.SelectedValue;
+            string tenHang = txtTenHang.Text;
+            string donGia = txtDonGia.Text;
+            string donViTinh = txtDonViTinh.Text;
+
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "DELETE FROM Hang WHERE MaHang = @MaHang";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaHang", maHang);
+
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            txtTenHang.Text = String.Empty;
+            txtDonGia.Text = String.Empty;
+            txtDonViTinh.Text = String.Empty;
+            LoadTable();
+        }
+
+        protected void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string searchText = txtTimKiem.Text.Trim();
+
+            using (OleDbConnection conn = connectDataBase())
+            {
+                string query = "SELECT * FROM Hang WHERE MaHang LIKE @SearchText OR TenHang LIKE @SearchText";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+
+                conn.Open();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                gvTatCa.DataSource = dt;
+                gvTatCa.DataBind();
+            }
+        }
+    }
+}
